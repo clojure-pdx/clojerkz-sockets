f0de139e46d7b73aad4f15de43a6d335422fb858
Jed <jed.clinger@buildingenergy.com>
2014-03-06 18:36:05 -0800

    Adds echo function and refactors to return core.async channels.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojerkz-sockets "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
--- src/clojerkz_sockets/core.clj
+++ src/clojerkz_sockets/core.clj
@@ -1,13 +1,32 @@
 (ns clojerkz-sockets.core
+  (:require [clojure.core.async :refer [go chan map< put! >! <!]])
   (:import [java.net.ServerSocket]))
 
 (defn start-server [& {:keys [port]}]
   (let [server-sock (java.net.ServerSocket. port)
-        sock (.accept server-sock)
-        in-stream (.getInputStream sock)
-        out-stream (.getOutputStream sock)]
+        sock (future (.accept server-sock))
+        in-stream (future (.getInputStream @sock))
+        out-stream (future (.getOutputStream @sock))
+        in (chan)]
     (println "listening on port 8080")
-    {:in in-stream
+    (go
+     (let [txt (slurp @in-stream)]
+       (>! in txt)))
+    {:in in
      :out out-stream
      :server server-sock
-     :socket sock}))
+     :socket sock
+     :shutdown (fn []
+                 (.close @in-stream)
+                 (.close @out-stream)
+                 (.close @sock)
+                 (.close server-sock))}))
+
+(defn shutdown [{:keys [shutdown]}]
+  (shutdown))
+
+(defn echo [{:keys [in]}]
+  (go
+   (while true
+     (let [msg (<! in)]
+       (println (str "received message: " msg))))))
